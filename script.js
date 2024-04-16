dbf5599f0d31c4111501f3f280118223b6dc6d92
Radica <radica_bt@hotmail.com>
2024-04-17 01:15:39 +0200

    Task01-added

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const images = [
+    "img/image1.jpg",
+    "img/image2.jpg",
+    "img/image3.jpg",
+    "img/image4.jpg",
+  ];
+  const imageDisplay = document.getElementById("imageDisplay");
+  const previousBtn = document.getElementById("previousBtn");
+  const nextBtn = document.getElementById("nextBtn");
+
+  let currentIndex = 0;
+
+  function updateImage() {
+    imageDisplay.src = images[currentIndex];
+  }
+  previousBtn.addEventListener("click", function () {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = images.length - 1;
+    }
+    updateImage();
+  });
+
+  nextBtn.addEventListener("click", function () {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
+    }
+    updateImage();
+  });
+
+  updateImage();
+});
